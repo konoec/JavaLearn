c83e8149c76fdd69d169f792f057c4d97a28ad56
konoec <kono.caec@gmail.com>
2024-10-22 16:42:16 -0500

    Constantes y Variables

---
+++ src/Datos/Constantes.java
@@ -0,0 +1,8 @@
+package Datos;
+
+public class Constantes {
+    public static void main(String[] args) {
+        final double TIPO_CAMBIO = 1.0;
+        System.out.println(TIPO_CAMBIO);
+    }
+}
---
+++ src/Datos/Variables.java
@@ -0,0 +1,13 @@
+package Datos;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Inicializar la variable
+        char letra = 'K';
+        System.out.println(letra);
+
+        // Al ser una variable, su valor puede cambiar
+        letra = 'L';
+        System.out.println(letra);
+    }
+}
