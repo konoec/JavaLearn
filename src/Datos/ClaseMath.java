9e3e6520a47ec2900887d69ad9026eefed60f929
konoec <kono.caec@gmail.com>
2024-10-23 01:16:04 -0500

    Clase Math

---
+++ src/Datos/ClaseMath.java
@@ -0,0 +1,13 @@
+package Datos;
+
+public class ClaseMath {
+    public static void main(String[] args) {
+        // Raiz cuadrada
+        double raiz  = Math.sqrt(2.0);
+        System.out.println(raiz);
+
+        // Redondeo
+        int redondeo = Math.round(3.49F);
+        System.out.println(redondeo);
+    }
+}
---
+++ src/Practica/Ejercicio01.java
@@ -0,0 +1,12 @@
+package Practica;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        final double dolar = 1.137;
+        final double salario = 2125.70;
+
+        String nombre = "Manuel";
+        double salarioDolar = salario * dolar;
+        System.out.println("El salario en dólares de " + nombre + " es " + salarioDolar);
+    }
+}
